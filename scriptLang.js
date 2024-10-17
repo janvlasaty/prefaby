5e66c2af5054cd1405d9df2f42fe3776c482966a
Jan Vlasatý <jan.vlasaty@melowntech.com>
2024-10-17 13:38:51 +0200

    Refactor CSS to update link color and add underline in shows section

--- scriptLang.js
+++ scriptLang.js
@@ -1,17 +1,50 @@
 
 let language = "en";
+let translations = null;
 
 function displaySelectedLanguage(language) {
-  document.querySelectorAll('[data-lang]').forEach((el) => {
-    el.style.display = 'none';
-  })
-  document.querySelectorAll(`[data-lang=${language}]`).forEach((el) => {
-    el.style.display = 'block';
+  const els = document.querySelectorAll('[data-translation]')
+  els.forEach((el) => {
+    const key = el.getAttribute('data-translation');
+    const translation = getNotedTranslation(key);
+    console.log(key, translation);
+    if (!Array.isArray(translation)) {
+      el.innerHTML = translation[language];
+    } else {
+      // add same element to the end of parent node
+      const parentNode = el.parentNode;
+      parentNode.innerHTML = "";
+      translation.forEach((t) => {
+        const clone = el.cloneNode(true);
+        delete clone.dataset.translation;
+        clone.innerHTML = t[language];
+        parentNode.appendChild(clone);
+      })
+    }
   })
 }
-displaySelectedLanguage(language);
 
 function switchLanguage() {
   language = language === "en" ? "cs" : "en";
   displaySelectedLanguage(language);
 }
+
+function getNotedTranslation(key) {
+  let keys = key.split('.');
+  let data = translations;
+  while(keys.length > 0) {
+    let key = keys.shift();
+    data = data[key];
+  }
+  return data
+}
+
+fetch('./data/translations.json').then((response) => {
+  return response.json();
+}).then((data) => {
+  translations = data;
+  displaySelectedLanguage(language);
+}).catch((error) => {
+  console.log(error);
+  document.querySelector('#langSwitcher').style.display = 'none';
+})
